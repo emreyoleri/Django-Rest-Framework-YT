3006eca00924d5b33b36dad7007748685aa91b54
Emre Yoleri <emreyoleri01@gmail.com>
2022-07-01 21:07:23 +0300

    POST Request

--- news/api/views.py
+++ news/api/views.py
@@ -6,10 +6,18 @@ from news.models import Article
 from news.api.serializers import ArticleSerializer
 
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def article_list_create_api_view(request):
 
     if request.method == "GET":
         articles = Article.objects.filter(is_active=True)
-        serializer = ArticleSerializer(articles, many=True)  # error
+        serializer = ArticleSerializer(
+            articles, many=True)  # error => many true !
         return Response(serializer.data)
+
+    elif request.method == "POST":
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
