0230e9ecc45f2c3313154a56bda45a59f19048e9
Emre Yoleri <emreyoleri01@gmail.com>
2022-07-08 23:18:47 +0300

    Class views added in api urls

--- news/api/urls.py
+++ news/api/urls.py
@@ -2,7 +2,15 @@ from django.urls import path, include
 from news.api import views as api_views
 
 urlpatterns = [
-    path("articles/", api_views.article_list_create_api_view, name="article-list"),
-    path("articles/<int:pk>", api_views.article_detail_api_view,
+    path("articles/", api_views.ArticleListCreateAPIView.as_view(),
+         name="article-list"),
+    path("articles/<int:pk>", api_views.ArticleDetailAPIVIEW.as_view(),
          name="article-detail"),
 ]
+
+
+# urlpatterns = [
+#     path("articles/", api_views.article_list_create_api_view, name="article-list"),
+#     path("articles/<int:pk>", api_views.article_detail_api_view,
+#          name="article-detail"),
+# ]
