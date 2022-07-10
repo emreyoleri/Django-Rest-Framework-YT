21940c5c1519d1ddebc3ca8ea9a7da2a9eb9875a
Emre Yoleri <emreyoleri01@gmail.com>
2022-07-10 15:08:54 +0300

    Created journalist model and paired with article

--- news/models.py
+++ news/models.py
@@ -3,8 +3,18 @@ from django.db import models
 # Create your models here.
 
 
+class Journalist(models.Model):
+    name = models.CharField(max_length=120)
+    surname = models.CharField(max_length=120)
+    biography = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
+
+
 class Article(models.Model):
-    author = models.CharField(max_length=150)
+    author = models.ForeignKey(
+        Journalist, on_delete=models.CASCADE, related_name="articles")
     title = models.CharField(max_length=120)
     description = models.CharField(max_length=200)
     text = models.TextField()
