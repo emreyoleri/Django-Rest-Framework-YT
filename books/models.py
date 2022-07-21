1d1cf6439e4045e11605ed7854f02ccdc6531ef9
Emre Yoleri <emreyoleri01@gmail.com>
2022-07-21 16:21:39 +0300

    Book and Comment Model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Book(models.Model):
+
+    name = models.CharField(max_length=120)
+    author = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    date_of_creation = models.DateTimeField(auto_now_add=True)
+    date_of_update = models.DateTimeField(auto_now=True)
+    date_of_relase = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.name} - {self.author}"
+
+
+class Comment(models.Model):
+    book = models.ForeignKey(
+        Book, on_delete=models.CASCADE, related_name="comments")
+    owner_of_comment = models.CharField(max_length=255)
+    comment = models.TextField(blank=True, null=True)
+    date_of_creation = models.DateTimeField(auto_now_add=True)
+    date_of_update = models.DateTimeField(auto_now=True)
+    raiting = models.PositiveIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+
+    def __str__(self):
+        return f"{self.raiting} - {self.owner_of_comment}"
