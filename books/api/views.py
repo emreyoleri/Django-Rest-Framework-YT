8f763ca34480ffb19fb7b2e11040921e555437b9
Emre Yoleri <emreyoleri01@gmail.com>
2022-07-27 18:08:23 +0300

    Book serializer with GenericAPIView

---
+++ books/api/views.py
@@ -0,0 +1,19 @@
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin
+from books.api.serializers import BookSerializer, CommentSerializer
+from books.models import Book
+
+
+class BookSerializer(ListModelMixin, CreateModelMixin, GenericAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    # List
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    # Create
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
