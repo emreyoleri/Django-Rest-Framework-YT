31db9eb382dd5ba5eecbcd5b9e44faf10d9ca0de
Emre Yoleri <emreyoleri01@gmail.com>
2022-07-27 23:28:41 +0300

    Comment path for book

--- books/api/urls.py
+++ books/api/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("books/", api_views.BookListCreateAPIView.as_view(), name="book-list"),
     path("books/<int:pk>",
          api_views.BookDetailCreateAPIView.as_view(), name="book-detail"),
+    path("books/<int:book_pk>/comment",
+         api_views.CommentCreateAPIView.as_view(), name="comment"),
 ]
