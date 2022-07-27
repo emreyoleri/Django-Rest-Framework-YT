1d27b778e283f70ac237b478d81f4abfae547508
Emre Yoleri <emreyoleri01@gmail.com>
2022-07-28 00:22:27 +0300

    IsAdminUserReadOnly permission added in views

--- books/api/views.py
+++ books/api/views.py
@@ -4,6 +4,7 @@ from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework import generics
 from rest_framework.generics import get_object_or_404
 from rest_framework import permissions
+from books.api.permissions import IsAdminUserOrReadOnly
 
 from books.api.serializers import BookSerializer, CommentSerializer
 from books.models import Book, Comment
@@ -12,12 +13,13 @@ from books.models import Book, Comment
 class BookListCreateAPIView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [IsAdminUserOrReadOnly]
 
 
 class BookDetailCreateAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
 
 
 class CommentCreateAPIView(generics.CreateAPIView):
