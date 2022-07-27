43366e19c8ffa65fdfd4fafbaec9c72cef15314b
Emre Yoleri <emreyoleri01@gmail.com>
2022-07-27 23:24:27 +0300

    Comment view and perform_create

--- books/api/views.py
+++ books/api/views.py
@@ -2,19 +2,30 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
 
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 
 from books.api.serializers import BookSerializer, CommentSerializer
-from books.models import Book
+from books.models import Book, Comment
 
-# ? ConcreteViews
+
 class BookListCreateAPIView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-# ? ConcreteViews
+
 class BookDetailCreateAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+class CommentCreateAPIView(generics.CreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def perform_create(self, serializer):
+        book_pk = self.kwargs.get("book_pk")
+        book = get_object_or_404(Book, pk=book_pk)
+        return serializer.save(book=book)
 
 
 # ? GenericAPIView
