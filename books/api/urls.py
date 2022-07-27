95544f100635fac014bb264950a138435bba0549
Emre Yoleri <emreyoleri01@gmail.com>
2022-07-27 18:32:28 +0300

    Book detail url added in api urls

--- books/api/urls.py
+++ books/api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from books.api import views as api_views
 
 urlpatterns = [
-    path("books/", api_views.BookListCreateAPIView.as_view(), name="book-list")
+    path("books/", api_views.BookListCreateAPIView.as_view(), name="book-list"),
+    path("books/<int:pk>",
+         api_views.BookDetailCreateAPIView.as_view(), name="book-detail"),
 ]
