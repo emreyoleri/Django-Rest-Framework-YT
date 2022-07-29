5e648007e536d311e4fc60ff3754a8df7377b8a9
Emre Yoleri <emreyoleri01@gmail.com>
2022-07-30 00:13:51 +0300

    IsOwnerOfCommentOrReadOnly class created (Read Documantation)

--- books/api/permissions.py
+++ books/api/permissions.py
@@ -7,3 +7,10 @@ class IsAdminUserOrReadOnly(permissions.IsAdminUser):
     def has_permission(self, request, view):
         is_admin = super().has_permission(request, view)
         return request.method in permissions.SAFE_METHODS or is_admin
+
+
+class IsOwnerOfCommentOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user == obj.owner_of_comment
