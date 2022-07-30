338d2e9e028c93f086b00e9f2bdbf2e27747b572
Emre Yoleri <emreyoleri01@gmail.com>
2022-07-30 15:45:24 +0300

    Custom pagination created

---
+++ books/api/paginations.py
@@ -0,0 +1,7 @@
+from rest_framework.pagination import PageNumberPagination
+
+class SmallPagination(PageNumberPagination):
+    page_size = 5
+    
+class LargePagination(PageNumberPagination):
+    page_size = 5
