be616f283d4972f9573a490a60bb946eda052132
Emre Yoleri <emreyoleri01@gmail.com>
2022-07-30 17:39:01 +0300

    Models registered in admin dashboard

--- user_profiles/admin.py
+++ user_profiles/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from user_profiles.models import Profile, ProfileStatus
 
+admin.site.register(Profile)
+admin.site.register(ProfileStatus)
 # Register your models here.
