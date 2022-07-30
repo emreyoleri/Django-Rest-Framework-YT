1d4819c1af5efb9def69d22c15743f973211451e
Emre Yoleri <emreyoleri01@gmail.com>
2022-07-30 15:43:32 +0300

    Limit and offset pagination

--- book_market/settings.py
+++ book_market/settings.py
@@ -133,7 +133,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # AllowAny , IsAuthenticated , IsAdminUser , IsAuthenticatedOrReadOnly
 
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+#     'PAGE_SIZE': 10
+# }
+
+# REST_FRAMEWORK = {
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+#     'PAGE_SIZE': 20
+# }
