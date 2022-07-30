d14750065effb7424b0a652225d797eff50e403e
Emre Yoleri <emreyoleri01@gmail.com>
2022-07-30 17:22:41 +0300

    Created profile model

--- user_profiles/models.py
+++ user_profiles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="profile")
+    bio = models.CharField(max_length=300, blank=True, null=True)
+    city = models.CharField(max_length=120, blank=True, null=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.city}"
