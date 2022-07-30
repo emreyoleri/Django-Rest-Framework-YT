2267d73f4cc3a450e50ee4dd2373b31d8d8c26ad
Emre Yoleri <emreyoleri01@gmail.com>
2022-07-30 15:41:42 +0300

    Page number pagination

--- book_market/settings.py
+++ book_market/settings.py
@@ -132,3 +132,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # }
 
 # AllowAny , IsAuthenticated , IsAdminUser , IsAuthenticatedOrReadOnly
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
