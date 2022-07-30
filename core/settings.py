cb6e231e95aab5f01584c107cac64eacef655542
Emre Yoleri <emreyoleri01@gmail.com>
2022-07-30 17:00:19 +0300

    Packages added in settings.py

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "user_profiles.apps.UserProfilesConfig",
+    "rest_framework",
+    "django_extensions"
 ]
 
 MIDDLEWARE = [
