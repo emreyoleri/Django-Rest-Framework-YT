0f8c623623e126550ea6aec9cb58fc76f55dadd4
Emre Yoleri <emreyoleri01@gmail.com>
2022-07-31 13:55:58 +0300

    Profile Serializer

---
+++ user_profiles/api/serializers.py
@@ -0,0 +1,12 @@
+from dataclasses import fields
+from rest_framework import serializers
+from user_profiles.models import Profile, ProfileStatus
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+    image = serializers.ImageField(read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = "__all__"
