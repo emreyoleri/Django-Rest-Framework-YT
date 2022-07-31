51668773ae5a25796bef6c6070fcf6888edf6023
Emre Yoleri <emreyoleri01@gmail.com>
2022-07-31 15:47:58 +0300

    USer profiles urls added in main (Core) urls.

--- core/urls.py
+++ core/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include("user_profiles.api.urls")),
     path("api-auth/", include("rest_framework.urls")),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
 ]
