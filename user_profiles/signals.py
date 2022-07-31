aa51d7b431584b3778d5d6d11a5646c661949b0e
Emre Yoleri <emreyoleri01@gmail.com>
2022-07-31 13:49:46 +0300

    New signal when profile created

--- user_profiles/signals.py
+++ user_profiles/signals.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from user_profiles.models import Profile
+from user_profiles.models import Profile, ProfileStatus
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -9,3 +9,12 @@ def create_profile(sender, instance, created, **kwargs):
     print(instance.username, "__Created: ", created)
     if created:
         Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=Profile)
+def create_profile_status(sender, instance, created, **kwargs):
+    if created:
+        ProfileStatus.objects.create(
+            user_profile=instance,
+            status_message=f"New user: {instance.user.username} "
+        )
