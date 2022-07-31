db08b842931fdff50767f66207bd4f936a2bb5f0
Emre Yoleri <emreyoleri01@gmail.com>
2022-07-31 14:39:25 +0300

    Token paths

--- core/urls.py
+++ core/urls.py
@@ -14,15 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path("api/rest-auth/", include("rest_auth.urls")),
 ]
 
-from django.conf.urls.static import static
-from django.conf import settings
 
 if settings.DEBUG == True:
     urlpatterns += static(settings.MEDIA_URL,
