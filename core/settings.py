1457fc57a9eae93b13479fcb6a19c730a2d3fda9
Emre Yoleri <emreyoleri01@gmail.com>
2022-07-31 14:57:30 +0300

    Fixed bugs for old package

--- core/settings.py
+++ core/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "user_profiles.apps.UserProfilesConfig",
     "rest_framework",
     "rest_framework.authtoken",
-    "rest_auth",
+    "dj_rest_auth",
     "django_extensions",
 ]
 
--- core/urls.py
+++ core/urls.py
@@ -22,7 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
-    path("api/rest-auth/", include("rest_auth.urls")),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
 ]
 
 
